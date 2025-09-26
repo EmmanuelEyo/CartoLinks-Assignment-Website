501b665a53933fa475cafcd8f2fe6884b86668d3
EmmanuelEyo <eyoemmanuel180@gmail.com>
2025-09-26 15:57:11 +0100

    fixed footer

--- app/components/FooterTwo.tsx
+++ app/components/FooterTwo.tsx
@@ -12,7 +12,7 @@ const FooterTwo: React.FC = () => {
     <footer className="w-full bg-[#222222]">
       <div className="mx-auto md:px-6">
         <div className="flex items-center justify-between gap-4 py-3 sm:py-4 text-sm  sm:text-base">
-          <div className="flex items-center gap-3.5 w-full sm:w-auto justify-center sm:justify-start">
+          <div className="flex items-center gap-3.5 w-full sm:w-auto justify-start sm:justify-start">
             <div className="flex items-center justify-center w-12 h-12 rounded-md bg-black text-white text-sm font-semibold flex-shrink-0">
               <Logo />
             </div>
@@ -20,7 +20,7 @@ const FooterTwo: React.FC = () => {
             <div className="text-white lg:text-2xl md:text-lg font-normal">Krea AI</div>
           </div>
 
-          <div className="flex items-center gap-3 w-full sm:w-auto justify-center sm:justify-end">
+          <div className="flex items-center gap-3 w-full sm:w-auto justify-end sm:justify-end">
             <span className="lg:text-2xl md:text-lg text-slate-300 hidden sm:inline">curated by</span>
             <div className="flex items-center">
               <div className="h-4 sm:h-6 md:h-4 lg:h-5 xl:h-6">
